3a61678786da2c9ba42ceccb85456896e83df8d9
Test4bo <test4bo@gmail.com>
2019-03-20 22:17:29 -0400

    Handle empty input field Added reset button set to portrait view

--- app/src/main/java/com/bogale/tpcalculator/MainActivity.java
+++ app/src/main/java/com/bogale/tpcalculator/MainActivity.java
@@ -1,7 +1,8 @@
 package com.bogale.tpcalculator;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     double total = 0.0;
     double sheet_price = 0.0;
+    String errorMessage = "Tofu Vic buy price is 0.25 cents per sheet.";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +28,16 @@ public class MainActivity extends AppCompatActivity {
         displayTotal();
 
     }
+
+    /** Reset button is pressed
+     *
+     */
+    public void resetAll (View View){
+        resetTotal();
+        resetDisplay();
+    }
+
+
 
     /** calculate total
      *
@@ -45,38 +57,106 @@ public class MainActivity extends AppCompatActivity {
 
         EditText rolls = (EditText)findViewById(R.id.editNumberOfRolls);
         String get_rolls = rolls.getText().toString();
-        n_rool_fromET = n_rool_fromET + Double.parseDouble(get_rolls);
-
+        double aNumber = ParseDouble(get_rolls);
+        if (aNumber == 0)  {
+            errorMessage = "errorMessage = Frst 3 fields cannot be blank!";
+        }
+        else {
+            n_rool_fromET = n_rool_fromET + aNumber;
+        }
         EditText sheets = (EditText)findViewById(R.id.editSheetsPerRoll);
         String get_sheets = sheets.getText().toString();
-        n_sheet_fromET = n_sheet_fromET + Double.parseDouble(get_sheets);
+        double bNumber = ParseDouble(get_sheets);
+        if (bNumber == 0) {
+            errorMessage = "errorMessage = Frst 3 fields cannot be blank!";
+        }
+        else {
+            n_sheet_fromET = n_sheet_fromET + bNumber;
+        }
 
         EditText price = (EditText)findViewById(R.id.editPriceBeforediscount);
         String get_price = price.getText().toString();
-        n_price_fromET = n_price_fromET + Double.parseDouble(get_price);
+        double cNumber = ParseDouble(get_price);
+        if (cNumber == 0)  {
+            errorMessage = "Frst 3 fields cannot be blank!";
+        }
+        else{
+            n_price_fromET = n_price_fromET + cNumber;
+        }
 
         EditText coupon_1 = (EditText)findViewById(R.id.editCoupon_1);
         String get_coupon1 = coupon_1.getText().toString();
-        n_coupon1_fromET = n_coupon1_fromET + Double.parseDouble(get_coupon1);
+        n_coupon1_fromET = n_coupon1_fromET + ParseDouble(get_coupon1);
 
-        EditText coupon_2 = (EditText)findViewById(R.id.edit_coupon2);
+        EditText coupon_2 = (EditText)findViewById(R.id.editCoupon_2);
         String get_coupon2 = coupon_2.getText().toString();
-        n_coupon2_fromET = n_coupon2_fromET + Double.parseDouble(get_coupon2);
+        n_coupon2_fromET = n_coupon2_fromET + ParseDouble(get_coupon2);
 
         total = (n_price_fromET * ((100.0 - n_coupon1_fromET )/100) - n_coupon2_fromET );
-        sheet_price = (total / (n_rool_fromET *n_sheet_fromET ))*100;
+        if (total > 0) {
+            sheet_price = (total / (n_rool_fromET * n_sheet_fromET)) * 100;
+        }
+        else sheet_price = 0;
 
     }
 
+    /** Handle empty input
+     *
+     */
+
+    double ParseDouble(String strNumber) {
+        if (strNumber != null && strNumber.length() > 0) {
+            try {
+                return Double.parseDouble(strNumber);
+            } catch(Exception e) {
+                return -999.0;
+            }
+        }
+        else
+            return 0;
+
+        }
     /** Display the total price
      *
      */
+    private void resetDisplay()
+    {
+        TextView numberRolls = findViewById(R.id.editNumberOfRolls);
+        numberRolls.setText("");
+        TextView numberSheets = findViewById(R.id.editSheetsPerRoll);
+        numberSheets.setText("");
+        TextView priceActual = findViewById(R.id.editPriceBeforediscount);
+        priceActual.setText("");
+        TextView coupon_01 = findViewById(R.id.editCoupon_1);
+        coupon_01.setText("");
+        TextView coupon_02 = findViewById(R.id.editCoupon_2);
+        coupon_02.setText("");
+        TextView errorTextView = findViewById(R.id.errorMessage);
+        errorTextView.setTextColor(Color.BLACK);
+        errorTextView.setText("Fill some number and tap Calculate");
+        TextView totalTextView = findViewById(R.id.amountTotal);
+        totalTextView.setText("");
+        TextView sheetTextView = findViewById(R.id.amountSheet);
+        sheetTextView.setText("");
+
+    }
+
     private void displayTotal()
     {
-        TextView totalTextView = (TextView) findViewById(R.id.amountTotal);
+        TextView totalTextView = findViewById(R.id.amountTotal);
         totalTextView.setText("$ " + String.format("%.2f", total ));
-        TextView sheetTextView = (TextView) findViewById(R.id.amountSheet);
+        TextView sheetTextView = findViewById(R.id.amountSheet);
         sheetTextView.setText(String.format("%.2f",sheet_price) + " cents" );
+        TextView errorTextView = findViewById(R.id.errorMessage);
+        errorTextView.setTextColor(Color.RED);
+        errorTextView.setText(errorMessage);
     }
+
+    private void resetTotal(){
+        total = 0.0;
+        sheet_price = 0.0;
+
+    }
+
 
 }
